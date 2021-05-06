9b931831cc681725daf32a746ac5b9de72bd58b3
bhavani-71 <82405309+bhavani-71@users.noreply.github.com>
2021-05-06 15:14:11 +0530

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,6 +1,6 @@
 let d=prompt("Enter your Date of Birth(DD/MM/YYYY):");
 document.write(d);
-let diff=2021-parseInt(d.substr(6));
+let diff=2021-parseInt(d.substring(d.length-4));
 document.write(diff)
 if(diff>=18 && diff<=44){
     location.replace("https://selfregistration.cowin.gov.in");
